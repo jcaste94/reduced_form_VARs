94706f0fac8b547f849d69b22a75dd0dcc01a030
Juan Castellanos <juan.castellanos@eui.eu>
2020-02-06 13:30:25 -0500

    Graph of actual data in VAR.m (main)

--- VAR.m
+++ VAR.m
@@ -1,13 +1,13 @@
 %==========================================================================
-%                       VAR WITH MINNESOTA PRIOR                      
+%           ECONOMETRICS IV - SPRING 2020 - Prof. Schorfehide 
+%                       REDUCED-FORM VARs                     
 %
-%                      See README_VAR for details
 %
-%
-% Author: Luigi Bocola     lbocola@sas.upenn.edu
-% Date  : 06/20/2010
+% Author: Juan Castellanos Silvan (from Luigi Boccola's original code)
+% Date  : 02/04/2020
 %==========================================================================
 
+% Exercise 4: Means and 90% credible sets 
 
 %=========================================================================
 %                             HOUSEKEEPING
@@ -18,16 +18,10 @@ close all
 clear all
 clc
 
-l = path;
-path('Impulse Responses',path);
-path('Minnesota Prior',path);
-path('Data',path);
-
-
 %=========================================================================
 %         GENERATE DUMMY OBSERVATIONS FROM MINNESOTA PRIOR 
 %=========================================================================
- 
+mprior = 2;     % see results in hyperparam.m
 vm_dummy
 
 %=========================================================================
@@ -122,9 +116,64 @@ vm_mdd
 
 mdd = lnpYY;               % Marginal Data Density
 
+%=========================================================================
+%                        MEANS AND CREDIBLE SETS
+%=========================================================================
+
+lb = prctile(Phip(:,end,:),10);
+%ub = prctile()
+
+
+disp(['         ELAPSED TIME:   ', num2str(toc)]);
+
+elapsedtime=toc;
+
+%% FIGURES
 
 %=========================================================================
-%           FIGURE 1: LARGEST EIGENVALUE (Companion Form)
+%           FIGURE 1: EVOLUTION OF ACTUAL DATA
+%=========================================================================
+
+% Attributes 
+linesty={'-','-','-','-'};
+color={'k','b','r','g'};
+marker={'none','none','none','none'};
+legend_list = {'Output', 'Inflation', 'Interest rate', 'Inverse velocity'};
+
+% xaxis
+time = 1965:0.25:2005;
+
+figure(1);
+p = plot(time, YYact, 'linewidth', 1);
+size_p=size(p);
+grid on;
+for i=1:size_p(1);
+    set(p(i),'LineStyle',linesty{i},'Color',color{i},'Marker',marker{i});
+    xlabel("Time")
+end
+h=legend(legend_list, 'orientation', 'horizontal');
+set(h,'Fontsize',10);
+set(h,'Position',[0.45 0.01 0.1 0.05]);
+
+X = 29.7;                  % A4 paper size
+Y = 21.0;                  % A4 paper size
+xMargin = 1;               % left/right margins from page borders
+yMargin = 1;               % bottom/top margins from page borders
+xSize = X - 2*xMargin;     % figure size on paper (widht & hieght)
+ySize = Y - 2*yMargin;     % figure size on paper (widht & hieght)
+
+set(gcf, 'Units','centimeters', 'Position',[0 0 xSize ySize]/2)
+
+set(gcf, 'PaperUnits','centimeters')
+set(gcf, 'PaperSize',[X Y])
+set(gcf, 'PaperPosition',[xMargin yMargin xSize ySize])
+set(gcf, 'PaperOrientation','portrait')
+
+print -dpdf -r0 pRawData.pdf
+
+
+%=========================================================================
+%           FIGURE 2: LARGEST EIGENVALUE (Companion Form)
 %=========================================================================
 
 pnames = strvcat( 'Largest Eigenvalue (Recursive Average)',...
@@ -149,9 +198,3 @@ subplot(1,2,2), plot(x,density,'LineStyle','-','Color','b',...
         'LineWidth',2.5), hold on
 
 title(pnames(2,:),'FontSize',13,'FontWeight','bold');
-
-path=l;
-
-disp(['         ELAPSED TIME:   ', num2str(toc)]);
-
-elapsedtime=toc;
